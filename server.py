6ca7dcb3d06e712c75a1fc9039adf34248e45885
Anatoly <antauren@yandex.ru>
2020-03-13 03:59:52 +0300

    fill archivate function

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
+import argparse
+from functools import partial
+
 from aiohttp import web
 import aiofiles
 
-
-async def archivate(request):
-    raise NotImplementedError
+from utils import archivate
 
 
 async def handle_index_page(request):
@@ -12,10 +13,31 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='''\
+        Микросервис для скачивания файлов.
+        
+        Как запустить: python server.py -p test_photos
+        '''
+    )
+
+    parser.add_argument('-p', '--path_to_dirs', default='test_photos', help='путь к каталогам с файлами', type=str)
+    parser.add_argument('-l', '--logging', default=False, type=bool, help='включить логирование', choices=[True, False])
+    parser.add_argument('-d', '--delay', default=0, type=int, help='включить задержку ответа')
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
+    args = parse_args()
+
+    p_archivate = partial(archivate, dir_path=args.path_to_dirs, delay=args.delay, log=args.logging)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get('/archive/{archive_hash}/', p_archivate),
+
     ])
     web.run_app(app)
---
+++ utils.py
@@ -0,0 +1,56 @@
+import os
+
+import asyncio
+from aiohttp import web
+from aiohttp.web import HTTPNotFound
+
+import logging
+
+
+async def archivate(request, dir_path, delay=0, log=False):
+    if os.path.samefile(dir_path, '.') or os.path.samefile(dir_path, '..'):
+        raise HTTPNotFound(reason='путь не должен вести на ".." или "."')
+
+    if not os.path.exists(dir_path):
+        raise HTTPNotFound(reason='Архив не существует или был удален')
+
+    archive_hash = request.match_info['archive_hash']
+    dir_path = os.path.join(dir_path, archive_hash)
+
+    cmd = ['zip', '-r', '-', dir_path]
+
+    response = web.StreamResponse()
+
+    response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
+
+    await response.prepare(request)
+
+    proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+    try:
+        while True:
+            archive_chunk = await proc.stdout.readline()
+
+            if log:
+                logging.debug('Sending archive chunk ...')
+
+            if not archive_chunk:
+                break
+
+            await response.write(archive_chunk)
+
+            if delay > 0:
+                await asyncio.sleep(delay)
+
+    except (asyncio.CancelledError, ConnectionResetError, BrokenPipeError, RuntimeError):
+        if log:
+            logging.warning('Download was interrupted.')
+
+        raise KeyboardInterrupt
+
+    finally:
+        proc.kill()
+
+        response.force_close()
+
+    return response
